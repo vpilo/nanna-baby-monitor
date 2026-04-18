0d92eb41c5bd3c8302f8a81102c4ff7c2c7851b8
Valerio Pilo <vpilo@users.noreply.github.com>
2026-04-18 23:42:44 +0200

    feat: add appRelay entry point

---
+++ appRelay/src/desktopMain/kotlin/org/vpilo/babymonitor/relay/Main.kt
@@ -0,0 +1,20 @@
+package org.vpilo.babymonitor.relay
+
+import kotlinx.coroutines.runBlocking
+import org.koin.core.context.startKoin
+import org.vpilo.babymonitor.network.relay.DefaultNetworkRelayRepository
+import org.vpilo.babymonitor.network.relay.di.networkRelayKoinModule
+
+fun main() {
+    val koin = startKoin {
+        modules(networkRelayKoinModule)
+    }.koin
+
+    val relay = koin.get<DefaultNetworkRelayRepository>()
+    relay.start()
+
+    println("Relay running. Press Enter to stop.")
+    runBlocking { readLine() }
+
+    relay.stop()
+}
