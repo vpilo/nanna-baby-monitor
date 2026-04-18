7242ff844528409533a44c828411ffb92c248aa4
Valerio Pilo <vpilo@users.noreply.github.com>
2026-04-18 23:09:13 +0200

    feat: add network:relay build file

--- network/relay/build.gradle.kts
+++ network/relay/build.gradle.kts
@@ -1,1 +1,20 @@
-// Placeholder - to be implemented in Task 3
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+}
+
+kotlin {
+    jvm("desktop")
+
+    sourceSets {
+        val desktopMain by getting
+
+        desktopMain.dependencies {
+            implementation(project(":common"))
+            implementation(project(":network:common"))
+
+            implementation(libs.bundles.ktor.server)
+            implementation(libs.bundles.ktor.client)
+            implementation(libs.koin.core)
+        }
+    }
+}
