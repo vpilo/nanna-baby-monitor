c014eba97e0a5c145055ffad2d37497d3c1c76d2
Valerio Pilo <vpilo@users.noreply.github.com>
2026-05-29 11:14:49 +0200

    Add the Compose Stability Analyzer plugin for AS and for Gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,7 @@ plugins {
     alias(libs.plugins.androidLibrary) apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.compose) apply false
+    alias(libs.plugins.compose.stability.analyzer) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.detekt) apply false
--- camera/presentation/build.gradle.kts
+++ camera/presentation/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     alias(libs.plugins.androidLibrary)
     alias(libs.plugins.compose)
     alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.compose.stability.analyzer)
 }
 
 kotlin {
