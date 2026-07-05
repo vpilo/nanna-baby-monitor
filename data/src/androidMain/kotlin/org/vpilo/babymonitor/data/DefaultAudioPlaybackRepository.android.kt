20846fcbdf9b0356f96a621c459fdd62127dfa46
Valerio Pilo <vpilo@users.noreply.github.com>
2026-07-05 16:42:08 +0200

    Switch Android audio attributes to voice comm usage

--- data/src/androidMain/kotlin/org/vpilo/babymonitor/data/DefaultAudioPlaybackRepository.android.kt
+++ data/src/androidMain/kotlin/org/vpilo/babymonitor/data/DefaultAudioPlaybackRepository.android.kt
@@ -31,7 +31,7 @@ internal actual class DefaultAudioPlaybackRepository actual constructor() : Audi
                 .setAudioAttributes(
                     AudioAttributes
                         .Builder()
-                        .setUsage(AudioAttributes.USAGE_MEDIA)
+                        .setUsage(AudioAttributes.USAGE_VOICE_COMMUNICATION)
                         .setContentType(AudioAttributes.CONTENT_TYPE_SPEECH)
                         .build(),
                 ).setAudioFormat(
