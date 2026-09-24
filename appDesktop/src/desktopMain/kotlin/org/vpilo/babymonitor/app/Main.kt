0abbf43bf1074a7a62110fea34cf9fbf6a2cf752
Valerio Pilo <vpilo@users.noreply.github.com>
2026-09-24 17:23:40 +0200

    Remove the stupid dialog on crash on desktop

--- appDesktop/src/desktopMain/kotlin/org/vpilo/babymonitor/app/Main.kt
+++ appDesktop/src/desktopMain/kotlin/org/vpilo/babymonitor/app/Main.kt
@@ -1,7 +1,11 @@
 package org.vpilo.babymonitor.app
 
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.LocalWindowExceptionHandlerFactory
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowExceptionHandler
+import androidx.compose.ui.window.WindowExceptionHandlerFactory
 import androidx.compose.ui.window.application
 import babymonitor.appcommon.generated.resources.Res
 import babymonitor.appcommon.generated.resources.app_icon
@@ -9,6 +13,18 @@ import babymonitor.appcommon.generated.resources.app_name
 import org.jetbrains.compose.resources.painterResource
 import org.jetbrains.compose.resources.stringResource
 import org.vpilo.babymonitor.settings.model.repository.SettingsRepository
+import kotlin.system.exitProcess
+
+@OptIn(ExperimentalComposeUiApi::class)
+private val exceptionHandlerFactory =
+    WindowExceptionHandlerFactory {
+        WindowExceptionHandler { throwable ->
+            Thread
+                .getDefaultUncaughtExceptionHandler()
+                ?.uncaughtException(Thread.currentThread(), throwable)
+            exitProcess(1)
+        }
+    }
 
 private fun babyMonitorMain() {
     val koin = initializeKoin()
@@ -21,14 +37,17 @@ private fun babyMonitorMain() {
             exitApplication()
         }
 
-        Window(
-            onCloseRequest = ::onCloseRequestHandler,
-            title = stringResource(Res.string.app_name),
-            icon = painterResource(Res.drawable.app_icon),
-            state = rememberPersistedWindowState(settingsRepository),
-        ) {
-            CompositionLocalProvider(LocalQuitApplication provides ::exitApplication) {
-                App()
+        @OptIn(ExperimentalComposeUiApi::class)
+        CompositionLocalProvider(LocalWindowExceptionHandlerFactory provides exceptionHandlerFactory) {
+            Window(
+                onCloseRequest = ::onCloseRequestHandler,
+                title = stringResource(Res.string.app_name),
+                icon = painterResource(Res.drawable.app_icon),
+                state = rememberPersistedWindowState(settingsRepository),
+            ) {
+                CompositionLocalProvider(LocalQuitApplication provides ::exitApplication) {
+                    App()
+                }
             }
         }
     }
